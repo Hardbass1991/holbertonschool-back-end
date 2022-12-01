80692b18b53fe4a374524a3dbc367b53b55d5344
Hardbass1991 <midailsorridente@gmail.com>
2022-12-01 03:10:10 +0000

    added module that fetches data from api and exports it into json file

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module that fetches data and exports it in csv format"""
+import json
+from sys import argv
+import urllib.request
+
+# First we get the name of the employee
+url = "https://jsonplaceholder.typicode.com/users/"
+webURL = urllib.request.urlopen(url)
+data = webURL.read()
+JSON_object = json.loads(data.decode('utf-8'))
+
+username = JSON_object[int(argv[1]) - 1]["username"]
+
+# Then we fetch the tasks
+url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(argv[1])
+webURL = urllib.request.urlopen(url)
+data = webURL.read()
+
+tasks = json.loads(data.decode('utf-8'))
+
+dct = {}
+task_list = []
+for task in tasks:
+    d_task = {}
+    d_task["task"] = task["title"]
+    d_task["completed"] = task["completed"]
+    d_task["username"] = username
+    
+    task_list.append(d_task)
+
+dct[argv[1]] = task_list
+
+with open("{}.json".format(argv[1]), "w") as f:
+    f.write(json.dumps(dct))
